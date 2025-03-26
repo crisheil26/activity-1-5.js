e350c393929a8f8e6d7ff0d74b62b56bb1783b88
crisheil26 <altheacrisheild@gmail.com>
2025-03-26 15:09:03 +0800

    Update booleans.js

---
+++ booleans.js
@@ -0,0 +1,12 @@
+// Declare boolean variables
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+//log the result
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+
+// Declare a favorite number
+let favNumb = 26;
+
+// Add favNumb with undefined
+let undefinedAddition = (favNumb + undefined);
