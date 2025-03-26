a1b7445da32c7b28d129ce7b3897f7ed20814ea4
crisheil26 <altheacrisheild@gmail.com>
2025-03-26 16:13:02 +0800

    Update strings.js

---
+++ strings.js
@@ -0,0 +1,21 @@
+// 1. Create variable name (favActorFirstName) & store your fav Actor name.
+let favActorFirstName = "Leonardo";
+
+// 2. Create variable name (favActorLastName) & store the last name of (FA).
+let favActorLastName = "DiCaprio";
+
+// 3. Create variable name (fullName) & concatenate (favActorFirstName, favActorLastName)
+let fullName = favActorFirstName + " " + favActorLastName;
+lastname;console.log(fullname);
+
+// 4. Create variable name (uppercase) & CAPITALIZE your fav actor name.
+let uppercase = fullName.toUpperCase();
+console.log(favActorFirstName.toUpperCase());
+
+// 5. Create a variable name (message) & store `My favorite Actor Is (favActorName) & say something about your fav actor` name should be in UPPERCASE.
+let message = "My favorite actor is " + uppercase;
+console.log(message + fullName.toUpperCase());
+
+// 6. Now append this message to the (message variable) his best show is Silicon Valley.
+firstName += "his best show is Silicon Valley";
+console.log(firstName);
