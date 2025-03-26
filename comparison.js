b35b6491b77d9812c260a753be429356a3635ac9
crisheil26 <altheacrisheild@gmail.com>
2025-03-26 15:27:47 +0800

    Update comparison.js

---
+++ comparison.js
@@ -0,0 +1,28 @@
+// Declare favorite number variables
+let firstFavNumb = 6;
+let secondFavNumb = 26;
+
+// Perform comparison operations
+let isGreaterThan = firstFavNumb > secondFavNumb;
+console.log(firstFavNumb > secondFavNumb);
+
+let isLessThan = firstFavNumb < secondFavNumb;
+console.log(firstFavNumb < secondFavNumb);
+
+let isGreaterOrEqual = firstFavNumb >= secondFavNumb;
+console.log(favNumb >= secondFavNumb);
+
+let isLessOrEqual = firstFavNumb <= secondFavNumb;
+console.log(favNumb <= secondFavNumb);
+
+let isStrictEqual = firstFavNumb === secondFavNumb;
+console.log(favNumb === secondFavNumb);
+
+let isLooseEqual = firstFavNumb == secondFavNumb;
+console.log(firstFavNumb == secondFavNumb);
+
+let isStrictNotEqual = firstFavNumb !== secondFavNumb;
+console.log(firstFavNumb !== secondFavNumb);
+
+let isLooseNotEqual = firstFavNumb != secondFavNumb;
+console.log(firstFavNumb != secondFavNumb);
