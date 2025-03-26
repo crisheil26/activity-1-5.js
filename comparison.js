7e55ad5ac667b03bc8d7988bd06ca3cbf03e8f0d
crisheil26 <altheacrisheild@gmail.com>
2025-03-26 16:30:33 +0800

    Update comparison.js

--- comparison.js
+++ comparison.js
@@ -1,28 +1,39 @@
-// Declare favorite number variables
+// 1. Create variable name (firstFavNumb) & store your favorite number.
 let firstFavNumb = 6;
-let secondFavNumb = 26;
+console.log(firstFavNumb);
 
-// Perform comparison operations
+// 2. Create variable name (secondFavNumb) & store second fav Number.
+let secondFavNumb = 26;
+console.log(secondFavNumb);
+
+// 3. Check (firstFavNumb is greater then secondFavNumb)
 let isGreaterThan = firstFavNumb > secondFavNumb;
 console.log(firstFavNumb > secondFavNumb);
 
+// 4. Check (firstFavNumb is less then secondFavNumb)
 let isLessThan = firstFavNumb < secondFavNumb;
 console.log(firstFavNumb < secondFavNumb);
 
+// 5. Check (firstFavNumb is greater then & equal to secondFavNumb)
 let isGreaterOrEqual = firstFavNumb >= secondFavNumb;
 console.log(favNumb >= secondFavNumb);
 
+// 6. Check (firstFavNumb is less then & equal to secondFavNumb)
 let isLessOrEqual = firstFavNumb <= secondFavNumb;
 console.log(favNumb <= secondFavNumb);
 
+// 7. Check (firstFavNumb is equal to secondFavNumb) using strict equality operator.
 let isStrictEqual = firstFavNumb === secondFavNumb;
 console.log(favNumb === secondFavNumb);
 
+// 8. Check (firstFavNumb is equal to secondFavNumb) using loose equality operator.
 let isLooseEqual = firstFavNumb == secondFavNumb;
 console.log(firstFavNumb == secondFavNumb);
 
+// 9. Check (firstFavNumb is not equal to secondFavNumb) using strict non-equality operator.
 let isStrictNotEqual = firstFavNumb !== secondFavNumb;
 console.log(firstFavNumb !== secondFavNumb);
 
+// 10. Check is (firstFavNumb is not equal to secondFavNumb) using loose non-equality operator.
 let isLooseNotEqual = firstFavNumb != secondFavNumb;
 console.log(firstFavNumb != secondFavNumb);
